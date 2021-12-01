75dbc56a170276e6f46cbc4ddcc08be00a9e2dbf
KartheekVajrala <kartheekvajrala@gmail.com>
2021-12-01 14:44:38 +0530

    first

---
+++ index.js
@@ -0,0 +1,56 @@
+
+for(var i = 0;i<document.getElementsByClassName('drum').length;i++){
+  document.getElementsByClassName('drum')[i].addEventListener('click', function(){triggerSound(this.innerHTML);anim(this.innerHTML);});
+}
+
+document.addEventListener('keypress',function(event){triggerSound(event.key);anim(event.key);});
+
+function triggerSound(text){
+  switch (text) {
+    case "w":
+    var soun = new Audio('sounds/tom-1.mp3');
+    soun.play();
+      break;
+
+    case "a":
+    var soun = new Audio('sounds/tom-2.mp3');
+    soun.play();
+      break;
+
+    case "s":
+    var soun = new Audio('sounds/tom-3.mp3');
+    soun.play();
+      break;
+
+    case "d":
+    var soun = new Audio('sounds/tom-4.mp3');
+    soun.play();
+      break;
+
+    case "j":
+    var soun = new Audio('sounds/snare.mp3');
+    soun.play();
+      break;
+
+    case "k":
+    var soun = new Audio('sounds/crash.mp3');
+    soun.play();
+      break;
+
+    case "k":
+    var soun = new Audio('sounds/kick-bass.mp3');
+    soun.play();
+    console.log(this.innerHTML);
+      break;
+
+    default:
+    console.log(this.innerHTML);
+
+  }
+}
+
+function anim(text){
+  var pressedButton = document.querySelector("."+text);
+  pressedButton.classList.add('pressed');
+  setTimeout(function(){pressedButton.classList.remove('pressed');},100);
+}
